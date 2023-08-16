05ca5bac652c14c4e4a92d8c4de49e37313fa5e2
bainaryho <rkswl233@naver.com>
2023-08-16 15:27:58 +0900

    add group

--- lion_app/Forum/admin.py
+++ lion_app/Forum/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Topic, Post
+from .models import Topic, Post, TopicGroupUser
 
 # Register your models here.
 admin.site.register(Topic)
 admin.site.register(Post)
+admin.site.register(TopicGroupUser)
--- lion_app/Forum/models.py
+++ lion_app/Forum/models.py
@@ -9,6 +9,7 @@ class Topic(models.Model):
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    posts: models.QuerySet["Post"]
 
     def __str__(self):
         return self.name
@@ -24,3 +25,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class TopicGroupUser(models.Model):
+    class groupChoices(models.IntegerChoices):
+        common = 0
+        admin = 1
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    group = models.IntegerField(
+        default=0, choices=groupChoices.choices
+    )  # 0 = common, 1 = admin
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
