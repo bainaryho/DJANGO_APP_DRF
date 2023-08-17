48f5f07e94cb338c1557766263d54d3ac7ebfa30
bainaryho <rkswl233@naver.com>
2023-08-17 10:11:24 +0900

    [추가] Forum.views: 생성시 권한 필터

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from requests import Response
+from rest_framework import viewsets, status
 from drf_spectacular.utils import extend_schema
 
-from .models import Topic, Post
+from .models import Topic, Post, TopicGroupUser
 from .serializers import TopicSerializer, PostSerializer
 
 
@@ -19,3 +22,23 @@ class TopicViewSet(viewsets.ModelViewSet):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def create(self, request, *args, **kwargs):
+        # 권한 그룹 체크 후 글을 작성합니다
+        # 유저가 권한을 가지고 있으면 글을 작성하게 해줌
+        user = request.user
+        data = request.data
+        topic_id = data.get("topic")
+        topic = get_object_or_404(Topic, id=topic_id)
+        if topic.is_private:
+            qs = TopicGroupUser.objects.filter(
+                group__lte=TopicGroupUser.groupChoices.common,
+                topic=topic,
+                user=user,
+            )
+            if not qs.exists():
+                return Response(
+                    status=status.HTTP_401_UNAUTHORIZED, data="유저는 해당 토픽에 글을 쓸 권한이 없습니다"
+                )
+
+        return super().create(request, *args, **kwargs)
